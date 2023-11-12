c2992442d5525991dd1c64d2128efbdebcdf7b48
maxtres <93056030+NourStudios@users.noreply.github.com>
2023-11-12 19:28:27 +0200

    Add files via upload

--- training.py
+++ training.py
@@ -13,32 +13,28 @@ def get_combined_value(pixel):
         raise ValueError(f"Unsupported image mode ({len(pixel)} channels). Only RGB, RGBA, and grayscale images are supported.")
 
 def train_model(image_folder, model_folder, learned_name):
-    learned_folder = os.path.join(model_folder, learned_name)
-
-    if not os.path.exists(learned_folder):
-        os.makedirs(learned_folder)
-
     for filename in os.listdir(image_folder):
-        if filename.endswith(('.jpg', '.jpeg', '.png')):
+        if filename.endswith('.png'):
             image_path = os.path.join(image_folder, filename)
             img = Image.open(image_path)
             width, height = img.size
 
-            with open(os.path.join(learned_folder, f"{filename}_results.txt"), "w") as result_file:
-                for y in range(height):
-                    for x in range(width):
-                        pixel = img.getpixel((x, y))
-                        combined_value = get_combined_value(pixel)
-                        result_file.write(f"Pixel {i + 1}: Combined Value = {combined_value}\n")
+            combined_values = []
 
-if __name__ == "__main__":
+            for y in range(height):
+                for x in range(width):
+                    pixel = img.getpixel((x, y))
+                    combined_value = get_combined_value(pixel)
+                    combined_values.append(combined_value)
+
+            result_path = os.path.join(model_folder, f"{filename}_{learned_name}_results.txt")
+            with open(result_path, "w") as result_file:
+                for i, combined_value in enumerate(combined_values):
+                    result_file.write(f"Pixel {i + 1}: Combined Value = {combined_value:.2f}\n")
+
+if __name__ == "__main__":?
     image_folder = input("Enter the folder containing the training images: ")
     model_folder = input("Enter the model folder: ")
     learned_name = input("Enter the name of what it has learned: ")
-
-    model_folder = os.path.join(os.getcwd(), model_folder)
-
-    if not os.path.exists(model_folder):
-        os.makedirs(model_folder)
 
     train_model(image_folder, model_folder, learned_name)
